fa82dcd370eab3345d4a8b154734ffb0f0afd7e2
Alex Kinch <alexthelion335@gmail.com>
2021-09-21 22:09:25 -0400

    Added searching
    Can now search for YouTube videos, and $play can be abbreviated to $p.

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ const {
 	token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
-//const JsSIP = require('jssip');
+const ytsr = require('ytsr');
 var target = ' ';
 const fs = require('fs');
-
-//web socket?
-//var socket = new JsSIP.WebSocketInterface('wss://192.168.1.133');
-
-//sip config
-/*var configuration = {
-  sockets  : [ socket ],
-  uri      : 'sip:3723@192.168.1.133',
-  password : '1234',
-  register : true
-};*/
 
 
 //log in
@@ -44,8 +33,7 @@ client.on('message', async message => {
   if (!message.content.startsWith(prefix)) return;
 
   //play command
-  if (message.content.startsWith(`${prefix}play`)) {
-    //message.channel.send("You have entered the call comand");
+  if (message.content.startsWith(`${prefix}play`) || message.content.startsWith(`${prefix}p`)) {
     execute(message);
     return;
   }
@@ -55,9 +43,9 @@ client.on('message', async message => {
     message.channel.send("I have been disconnected from the voice channel.");
   }
   if (message.content.startsWith(`${prefix}help`)) {
-    message.channel.send("$help - displays this help message\n$about - gives an about message\n$play [url] - plays a song with url(only audio files and YouTube links are supported)\n$stop - makes Tocando leave the channel");
+    message.channel.send("$help - displays this help message\n$about - gives an about message\n$play [url] - plays a song with url(only audio files and YouTube links are supported) or searches YouTube for the song\n$stop - makes Tocando leave the channel");
   if (message.content.startsWith(`${prefix}about`)) {
-    message.channel.send("The Tocando bot will play any audio file you have the link to.\nTo be added: Queue\nTocando-Bot v1.1b\n(c) Alex Kinch 2021");
+    message.channel.send("The Tocando bot will play any audio file you have the link to.\nTo be added: Queue\nTocando-Bot v1.2\n(c) Alex Kinch 2021");
   }
 })
 
@@ -73,22 +61,24 @@ async function execute(message) {
     message.channel.send("I need to have permissions to join and speak!");
     return;
   }
-  //message.channel.send("Calling");
+
   var url = args[1];
-  /*var ua = new JsSIP.UA(configuration);
-  //start sip connection
-  //var connection = await voiceChannel.join();
-  voiceChannel.join();
-  ua.start();
-  ua.call(target);*/
+
     var connection = await voiceChannel.join();
     
     if (url.includes("youtube") || url.includes("youtu.be")) {
 	const dispatcher = connection.play(await ytdl(url), { type: 'opus' });
+    } else if (url.includes("http")) {
+    	const dispatcher = connection.play(url);
     } else {
-	const dispatcher = connection.play(target);
+	//remove the first item from the array ($play)
+	args.shift();
+	//change array into string with spaces in between
+	const videoUrl = await ytsr(args.join(" "));
+	//get youtube dl link for the first item in the search results
+	const search = await ytdl(videoUrl.items[0].url);
+	const dispatcher = connection.play(search);
+	message.channel.send("Now Playing: " + videoUrl.items[0].url);
     }
 	
-  /*const audio = connection.receiver.createStream('448560538142769163', { mode: 'pcm' });
-  audio.pipe(fs.createWriteStream('user_audio'));*/
 }
